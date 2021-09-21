7d17b97647ae3694cf006f4dfe380b0bc9997ba2
YogeeshaBayari <70871926+YogeeshaBayari@users.noreply.github.com>
2021-09-21 14:40:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,6 +45,6 @@ app.post("/", function (req, res) {
     });
 });
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("server is running on port 3000");
 });
